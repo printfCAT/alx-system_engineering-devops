36fc90db8f9097c2a89182ed79e3b3fc8b51df39
printfCAT <cliffomanyo@gmail.com>
2023-08-17 14:54:51 -0700

    task 0 fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,13 @@
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
+  command     => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path        => ['/usr/local/bin', '/bin'],
+  refreshonly => true,  # Only run when needed (when nginx.conf is modified)
 } ->
 
 # Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+service { 'nginx':
+  ensure  => 'running',   # Ensure Nginx service is running
+  enable  => true,        # Start Nginx service on boot
+  require => Exec['fix--for-nginx'],  # Wait for the file update before restarting
 }
